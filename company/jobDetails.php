c41fde842f44a293cde4451d36d28c6296f7636b
Muhammad Arshad <muhammadarshad.ciit@gmail.com>
2018-01-21 20:03:23 +0500

    Version 1 Login for company

---
+++ company/jobDetails.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <meta charset="utf-8"> 
+        <meta name="description" content=""> 
+        <meta name="author" content="pixelcave"> 
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0"> 
+        <title>Job Details</title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="../css/custom.css"> 
+        <link href="../css/font-awesome.min.css" rel="stylesheet"> 
+        <link rel="stylesheet" href="../css/findWork.css"> 
+    </head>     
+    <body> 
+        <?php require "header.php"; ?>         
+        <!-- END Site Header -->         
+        <div id="page-wrappers"> 
+            <!-- Section Find-Work Start -->             
+            <section class="section-job-details"> 
+                <div class="container">
+                    <br>
+                    <h3>We need an android application GUI</h3>
+                    <br>
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="main-boxs wh-box">
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                        <div class="cont cont-active">
+                                            <center><a href="" class="txt">VIEW JOB POST</a></center>
+                                            <div class="arrac"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                        <div class="cont">
+                                            <center><a href="">INVITE FREELANCERS</a></center>
+                                            <div class="arr"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                        <div class="cont">
+                                            <center><a href="">REVIEW PROPOSALS</a></center>
+                                            <div class="arr"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                        <div class="cont">
+                                            <center><a href="">HIRE (0)</a></center>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>  
+                        </div>
+                    </div>
+                    <br>
+                    <!-- Jobs Details  Pages -->
+                    <br>
+                    <div class="main-box" style="padding:0;">
+                        <div class="row">
+                            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
+                                
+                                <div class="panel panel-default job-details-panel"  style="margin:0;">
+                                    <div class="panel-body">
+                                        <!-- Job Details -->
+                                        <div class="job-details-contents">
+                                            <h3>Job Details</h3>
+                                            <br>
+                                            <p><button class="btn btn-xs">Mobile Development</button>&nbsp;
+                                            <i class="fa fa-angle-right" aria-hidden="true"></i>&nbsp;
+                                            <button class="btn btn-xs">Android Development</button>&nbsp;
+                                            Posted 1 min ago</p>
+                                            <div class="row">
+                                                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12" style="border-right: 1px solid #E1E1E1;">
+                                                    <div class="job-descriptions">
+                                                        <br>
+                                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et quod est munus, quod opus sapientiae? Vitiosum est enim in dividendo partem in genere numerare. Quis est, qui non oderit libidinosam, protervam adolescentiam? Quid, quod res alia tota est? Videmus igitur ut conquiescere ne infantes quidem possint. Duo Reges: constructio interrete.
+                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et quod est munus, quod opus sapientiae? Vitiosum est enim in dividendo partem in genere numerare. Videmus igitur ut conquiescere ne infantes quidem possint. Duo Reges: constructio interrete. </p>
+                                                        <br>
+                                                        <h3>Attachments (3)</h3>
+                                                        <p><i class="fa fa-paperclip" aria-hidden="true"></i><a href="" class="text-green"> gui1.png (570 kb)</a></p>
+                                                        <p><i class="fa fa-paperclip" aria-hidden="true"></i><a href="" class="text-green"> gui2.png (550 kb)</a></p>
+                                                        <p><i class="fa fa-paperclip" aria-hidden="true"></i><a href="" class="text-green"> gui3.png (590 kb)</a></p>
+                                                        <br>
+                                                        <p>Project Stage: Design</p>
+                                                        <p>Project Type: One-time project</p>
+                                                        <p>You will be asked to answer the following questions when submitting a proposal:</p>
+                                                        <p style="margin-left:20px;">1. Have you read the job description and seen the attachments?</p>
+                                                        <br>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                                    <div class="job-types">
+                                                        <br>
+                                                        <p><i class="fa fa-tag" aria-hidden="true"></i>&nbsp; $40</p>
+                                                        <p style="margin-left:25px;" class="text-muted">Fixed Price</p>
+                                                        <br>
+                                                        <p>$$$ Expert Level</p>
+                                                        <p style="margin-left:25px;" class="text-muted">I am willing to pay higher rates <br> for the most experieced freelancers</p>
+                                                        <br>
+                                                        <p><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp; January 20, 2018</p>
+                                                        <p style="margin-left:25px;" class="text-muted">I am willing to pay higher rates <br> for the most experieced freelancers</p>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                        </div>
+                                        <hr>
+                                        <div class="job-details-contents">
+                                            <h3>Skills and Experties</h3>
+                                            <div class="row">
+                                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                    <br>   
+                                                    <h4>Mobile Devices</h4>
+                                                    <p><button class="btn btn-xs">Phone</button>&nbsp;
+                                                    <button class="btn btn-xs">Tablet</button></p>
+                                                    <br>
+                                                    <h4>Google Play Store category</h4>
+                                                    <p><button class="btn btn-xs">House & Home</button></p>
+                                                    <br>
+                                                </div>
+                                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                    <br>   
+                                                    <h4>Android Programming Languages</h4>
+                                                    <p><button class="btn btn-xs">Java</button>&nbsp;
+                                                    <button class="btn btn-xs">xml</button></p>
+                                                    <br>
+                                                    <h4>Others</h4>
+                                                    <p><button class="btn btn-xs">Android App Development</button></p>
+                                                    <br>
+                                                </div>
+                                            </div>
+                                            <br>
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                    <h4>Prefered Qualinfications</h4>
+                                                    <br>   
+                                                    <p><span class="text-muted">Freelancer Type:</span> Freelancers Only</p>
+                                                    <p><span class="text-muted">Job Success Score:</span> At least 90%</p>
+                                                    <p><span class="text-muted">Include Rising Talent:</span> Yes</p>
+                                                    <p><span class="text-muted">Engilsh Level:</span> Fluent - Has completed Command on this language</p>
+                                                    <p><span class="text-muted">Work Hour:</span> <i class="fa fa-question-circle text-green" aria-hidden="true"></i> At least 1 hour</p>
+                                                    <br>
+                                                </div>
+                                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                                    <h4>Activity on this Job</h4>
+                                                    <br>   
+                                                    <p><span class="text-muted">Proposals:</span> 5 to 10 <i class="fa fa-question-circle text-green" aria-hidden="true"></i></p>
+                                                    <p><span class="text-muted">Last Veiwed by Client:</span> 1 month ago <i class="fa fa-question-circle text-green" aria-hidden="true"></i></p>
+                                                    <p><span class="text-muted">Interviewing:</span> 1</p>
+                                                    <p><span class="text-muted">Invites Sent:</span> 1</p>
+                                                    <p><span class="text-muted">Unanswered Invites:</span> 1</p>
+                                                    <br>
+                                                </div>
+                                            </div>
+                                            <h4>Bid Range - High $50.00 | Avg $40.00 | Low $30.00</h4>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <!-- Jobs Contents -->
+                                </div>
+                                
+                            </div>
+                            
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                <!-- Sidebar-nav -->
+                                <br>
+                                <div class="list-group job-feed-categories">
+                                    <a href="#" class="list-group-item"><i class="fa fa-eye" aria-hidden="true"></i> Veiw Posting</a>
+                                    <a href="#" class="list-group-item"><i class="fa fa-pencil" aria-hidden="true"></i> Edit Posting</a>
+                                    <a href="#" class="list-group-item"><i class="fa fa-times" aria-hidden="true"></i> Remove Posting</a>
+                                    <a href="#" class="list-group-item"><i class="fa fa-thumb-tack" aria-hidden="true"></i> Reost Job</a>
+                                    <a href="#" class="list-group-item"><i class="fa fa-search-minus" aria-hidden="true"></i> Make Private</a>
+                                </div>
+                                <br>
+                                <!-- Sidebar-nav End -->
+                            </div>
+                            
+                        </div>
+                    
+                    </div>
+                    
+                    
+                </div>
+                <br>
+            </section>
+            <br><br> 
+        </div>   
+        <!-- End Section Find-Work -->  
+        
+        <!-- Footer Start -->         
+        <?php require "../inc/footer.php"; ?>         
+        <!-- End Footer -->  
+               
+        <!--<script src="../js/jquery.js"></script>-->         
+        <script src="../js/bootstrap.min.js"></script>         
+        <script src="../js/app.js"></script>         
+        <script src="../js/dragFiles.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+    </body>     
+</html>
---
+++ company/myjob - ijaz.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+    <head> 
+        <meta charset="utf-8"> 
+        <meta name="description" content=""> 
+        <meta name="author" content="pixelcave"> 
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0"> 
+        <title>My job</title>         
+        <link rel="stylesheet" href="../css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="../css/custom.css"> 
+        <link href="../css/font-awesome.min.css" rel="stylesheet">
+    </head>     
+    <body> 
+        <?php require "../inc/header.php"; ?>         
+        <!-- END Site Header -->         
+        <div id="page-wrappers"> 
+            <!-- Section Porposal -->             
+            <section class="categories-section site-section-top">
+                <div class="container"> 
+                   <br> 
+    				<div class="panel panel-default prosal-container-padding">
+    				    <div class="panel-heading panel-padding post_title" >Postings 
+							<button type="button" class="btn pull-right m1" >Rehire a freelancer </button>
+							<button type="button" class="btn btn-success pull-right">Post a New job</button>
+							
+						</div>
+    				    <div class="panel-body  panel-padding">
+            				<table class="table">
+            					<thead class="table-responsive">
+            					    <tr>
+            						    <th class="col-sm-9">JOB TITLE</th>
+            						    <th class="col-sm-3">PROPOSALS</th>
+										<th class="col-sm-3">MESSAGED</th>
+            						    <th class="col-sm-3">OFFERS/HIRES</th>
+										<th class="col-sm-3">STATUS</th>
+            						    
+            					  </tr>
+            					</thead>
+            					<tbody>
+            					    <tr>
+                						<td  style="color:green">WEB based App</td>
+                						<td>i need a web based project for my company</td>
+										<td >i need a web based project for my company   </td>
+                						<td>NO</td>
+										<td >public</td>
+            					    </tr>
+									<tr>
+                						<td style="color:green">Android based App</td>
+                						<td>i need a Andriod based project for my company</td>
+										<td >i need a Android based project for my company   </td>
+                						<td>NO</td>
+										<td >public</td>
+            					    </tr>
+									<tr>
+                						<td style="color:green">IOS based App</td>
+                						<td>i need a IOS based project for my company</td>
+										<td >i need a IOS based project for my company   </td>
+                						<td>NO</td>
+										<td >public</td>
+            					    </tr>
+            					   
+            					</tbody>
+            				</table>
+            			</div>
+						<div class="panel-footer"><h3> <span style="color:green"> 
+						<a href="#" ><h3><span style="color:green">View all Job  postings</span>( including  filled and close jobs)</a><h3></div>
+    				</div>
+    				<br>
+                	<div class="panel panel-default prosal-container-padding">
+                		<div class="panel-heading panel-padding"><h3>Contacts <a href="#"  style="color:green">View full  contracts lister</a></h3></div>
+            			<div class="panel-body  panel-padding">
+							<p>Once you hire a student ,you can view here</p>								
+            			</div>
+                	</div>
+				</div>
+			</section>
+            <br>
+		</div>
+        <br>      
+        <!-- Footer Start -->         
+        <?php require "../inc/footer.php"; ?>          
+        <!-- End Footer -->         
+                
+        <script src="../js/bootstrap.min.js"></script>         
+        <script src="../js/app.js"></script>         
+        <script src="../js/dragFiles.js"></script>      
+    </body>     
+</html>
